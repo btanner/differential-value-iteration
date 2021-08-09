a566d8671a07fda4bc3a11f01db7d4acb7fc55f8
Brian Tanner <tanno@google.com>
2021-08-09 09:53:37 -0600

    Actually added the files from previous commit this time.

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""Sample program that runs a dvi algorithm on a sample MDP."""
+import numpy as np
+from dvi.algorithms import algorithms
+from dvi.environments import environments
+ 
+def run():
+  env = environments.ThreeLoopMRP()
+  alphas = np.arange(0.01, 1.01, 0.01)
+  convergence_flags = np.zeros(alphas.shape)
+  for _, alpha in enumerate(alphas):
+    print(f'Starting alpha:{alpha}', end=' ')
+    _, convergence,_ = algorithms.rvi_sync(3, env.P, env.R, max_iters=1000, ref_state=1, alpha=alpha) 
+    print(f'Converged? {convergence}')
+
+if __name__ == '__main__':
+  run()
