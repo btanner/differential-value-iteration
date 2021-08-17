b24eb155fd00932008e294165475f52aca3aa9f5
yiwan-rl <wan6@ualberta.ca>
2021-08-17 09:34:03 +0800

    Small changes

--- src/differential_value_iteration/import_test.py
+++ src/differential_value_iteration/import_test.py
@@ -4,13 +4,23 @@ from absl.testing import absltest
 from differential_value_iteration.algorithms import algorithms
 from differential_value_iteration.environments import environments
 
+
 class DifferentialValueIterationTest(absltest.TestCase):
 
   def test_algorithms_package_import(self):
-    self.assertTrue(algorithms.rvi_sync)
+    self.assertTrue(algorithms.RVI_Evaluation)
+    self.assertTrue(algorithms.DVI_Evaluation)
+    self.assertTrue(algorithms.MDVI_Evaluation)
+    self.assertTrue(algorithms.RVI_Control)
+    self.assertTrue(algorithms.DVI_Control)
+    self.assertTrue(algorithms.MDVI_Control1)
+    self.assertTrue(algorithms.MDVI_Control2)
 
   def test_environments_package_import(self):
-    self.assertTrue(environments.ThreeLoopMRP)
+    self.assertTrue(environments.mrp1)
+    self.assertTrue(environments.mrp2)
+    self.assertTrue(environments.mdp1)
+    self.assertTrue(environments.mdp2)
 
 
 if __name__ == '__main__':
