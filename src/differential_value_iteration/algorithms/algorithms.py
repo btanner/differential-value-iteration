54cf14ccf2aa773fb0d41af4b055475f110c25ff
Brian Tanner <tanno@google.com>
2021-08-19 12:24:55 -0600

    Plumbed dataclasses through to algorithms. Froze the dataclasses so that transition and rewards matrices are immutable. Added sanity checks to make sure transition matrices are valid. Updated num_states and num_actions properties (and comments) to reflect reality.

--- src/differential_value_iteration/algorithms/algorithms.py
+++ src/differential_value_iteration/algorithms/algorithms.py
@@ -4,28 +4,29 @@ import numpy as np
 # from differential_value_iteration.environments import MRP, MDP
 
 
-class RVI_Evaluation(object):
-  def __init__(self, mrp, v, alpha=1.0, ref_idx=0):
+class RVI_Evaluation:
+  def __init__(self, mrp, v, alpha: float = 1., ref_idx: int = 0):
     # assert type(mrp) is MRP
-    self.P = mrp.P.copy()
-    self.r = mrp.r.copy()
+    self.p = mrp.transitions
+    self.r = mrp.rewards
     self.init_v = v.copy()
     self.alpha = alpha
     self.idx = 0
     self.ref_idx = ref_idx
-    self.num_states = mrp.num_states()
+    self.num_states = mrp.num_states
     self.reset()
 
   def exec_sync(self):
     self.v += self.alpha * (
-          self.r + np.dot(self.P, self.v - self.v[self.ref_idx]) - self.v)
+        self.r + np.dot(self.p,
+                        self.v - self.v[self.ref_idx]) - self.v)
     return
 
   def exec_async(self):
     idx = self.idx
     self.v[idx] += self.alpha * (
-          self.r[idx] + np.dot(self.P[idx], self.v - self.v[self.ref_idx]) -
-          self.v[idx])
+        self.r[idx] + np.dot(self.p[idx], self.v - self.v[self.ref_idx]) -
+        self.v[idx])
     self.idx = (self.idx + 1) % self.num_states
     return
 
@@ -36,20 +37,20 @@ class RVI_Evaluation(object):
 class RVI_Control(object):
   def __init__(self, mdp, v, alpha=1.0, ref_idx=0):
     # assert type(mdp) is MDP
-    self.P = mdp.P.copy()
-    self.r = mdp.r.copy()
+    self.p = mdp.transitions
+    self.r = mdp.rewards
     self.init_v = v.copy()
     self.alpha = alpha
     self.idx = 0
     self.ref_idx = ref_idx
-    self.num_states = mdp.num_states()
-    self.num_actions = mdp.num_actions()
+    self.num_states = mdp.num_states
+    self.num_actions = mdp.num_actions
     self.reset()
 
   def exec_sync(self):
     temp_s_by_a = np.zeros((self.num_states, self.num_actions))
     for a in range(self.num_actions):
-      temp_s_by_a[:, a] = self.r[a] + np.dot(self.P[a], self.v - self.v[
+      temp_s_by_a[:, a] = self.r[a] + np.dot(self.p[a], self.v - self.v[
         self.ref_idx]) - self.v
     delta = np.max(temp_s_by_a, axis=1)
     self.v += self.alpha * delta
@@ -59,7 +60,7 @@ class RVI_Control(object):
     idx = self.idx
     temp_a = np.zeros(self.num_actions)
     for a in range(self.num_actions):
-      temp_a[a] = self.r[a][idx] + np.dot(self.P[a][idx],
+      temp_a[a] = self.r[a][idx] + np.dot(self.p[a][idx],
                                           self.v - self.v[self.ref_idx]) - \
                   self.v[idx]
     delta = np.max(temp_a)
@@ -74,25 +75,25 @@ class RVI_Control(object):
 class DVI_Evaluation(object):
   def __init__(self, mrp, init_v, init_r_bar, alpha=1.0, beta=1.0):
     # assert type(mrp) is MRP
-    self.P = mrp.P.copy()
-    self.r = mrp.r.copy()
+    self.p = mrp.transitions
+    self.r = mrp.rewards
     self.init_v = init_v.copy()
     self.init_r_bar = init_r_bar
     self.alpha = alpha
     self.beta = beta
     self.idx = 0
-    self.num_states = mrp.num_states()
+    self.num_states = mrp.num_states
     self.reset()
 
   def exec_sync(self):
-    delta = self.r - self.r_bar + np.dot(self.P, self.v) - self.v
+    delta = self.r - self.r_bar + np.dot(self.p, self.v) - self.v
     self.v += self.alpha * delta
     self.r_bar += self.beta * np.sum(delta)
     return
 
   def exec_async(self):
     idx = self.idx
-    delta = self.r[idx] - self.r_bar + np.dot(self.P[idx], self.v) - self.v[idx]
+    delta = self.r[idx] - self.r_bar + np.dot(self.p[idx], self.v) - self.v[idx]
     self.v[idx] += self.alpha * delta
     self.r_bar += self.beta * delta
     self.idx = (self.idx + 1) % self.num_states
@@ -106,21 +107,21 @@ class DVI_Evaluation(object):
 class DVI_Control(object):
   def __init__(self, mdp, init_v, init_r_bar, alpha=1.0, beta=1.0):
     # assert type(mdp) is MDP
-    self.P = mdp.P.copy()
-    self.r = mdp.r.copy()
+    self.p = mdp.transitions
+    self.r = mdp.rewards
     self.init_v = init_v.copy()
     self.init_r_bar = init_r_bar
     self.alpha = alpha
     self.beta = beta
     self.idx = 0
-    self.num_actions = mdp.num_actions()
-    self.num_states = mdp.num_states()
+    self.num_actions = mdp.num_actions
+    self.num_states = mdp.num_states
     self.reset()
 
   def exec_sync(self):
     temp_s_by_a = np.zeros((self.num_states, self.num_actions))
     for a in range(self.num_actions):
-      temp_s_by_a[:, a] = self.r[a] - self.r_bar + np.dot(self.P[a],
+      temp_s_by_a[:, a] = self.r[a] - self.r_bar + np.dot(self.p[a],
                                                           self.v) - self.v
     delta = np.max(temp_s_by_a, axis=1)
     self.v += self.alpha * delta
@@ -131,7 +132,7 @@ class DVI_Control(object):
     idx = self.idx
     temp_a = np.zeros(self.num_actions)
     for a in range(self.num_actions):
-      temp_a[a] = self.r[a][idx] - self.r_bar + np.dot(self.P[a][idx], self.v) - \
+      temp_a[a] = self.r[a][idx] - self.r_bar + np.dot(self.p[a][idx], self.v) - \
                   self.v[idx]
     delta = np.max(temp_a)
     self.v[idx] += self.alpha * delta
@@ -147,27 +148,27 @@ class DVI_Control(object):
 class MDVI_Evaluation(object):
   def __init__(self, mrp, init_v, init_r_bar, alpha=1.0, beta=1.0):
     # assert type(mrp) is MRP
-    self.P = mrp.P.copy()
-    self.r = mrp.r.copy()
+    self.p = mrp.transitions
+    self.r = mrp.rewards
     self.init_v = init_v.copy()
     self.init_r_bar = init_r_bar.copy()
     self.alpha = alpha
     self.beta = beta
-    self.num_states = mrp.num_states()
+    self.num_states = mrp.num_states
     self.idx = 0
     self.reset()
 
   def exec_sync(self):
-    self.r_bar = np.dot(self.P, self.r_bar)
-    delta = self.r - self.r_bar + np.dot(self.P, self.v) - self.v
+    self.r_bar = np.dot(self.p, self.r_bar)
+    delta = self.r - self.r_bar + np.dot(self.p, self.v) - self.v
     self.v += self.alpha * delta
     self.r_bar += self.beta * delta
     return
 
   def exec_async(self):
     idx = self.idx
-    self.r_bar[idx] = np.dot(self.P[idx], self.r_bar)
-    delta = self.r[idx] - self.r_bar[idx] + np.dot(self.P[idx], self.v) - \
+    self.r_bar[idx] = np.dot(self.p[idx], self.r_bar)
+    delta = self.r[idx] - self.r_bar[idx] + np.dot(self.p[idx], self.v) - \
             self.v[idx]
     self.v[idx] += self.alpha * delta
     self.r_bar[idx] += self.beta * delta
@@ -183,14 +184,14 @@ class MDVI_Control1(object):
   def __init__(self, mdp, init_v, init_r_bar, alpha=1.0, beta=1.0,
       threshold=0.01):
     # assert type(mdp) is MDP
-    self.P = mdp.P.copy()
-    self.r = mdp.r.copy()
+    self.p = mdp.transitions
+    self.r = mdp.rewards
     self.init_v = init_v.copy()
     self.init_r_bar = init_r_bar.copy()
     self.alpha = alpha
     self.beta = beta
-    self.num_states = mdp.num_states()
-    self.num_actions = mdp.num_actions()
+    self.num_states = mdp.num_states
+    self.num_actions = mdp.num_actions
     self.idx = 0
     self.threshold = threshold
     self.reset()
@@ -200,15 +201,15 @@ class MDVI_Control1(object):
     num_actions = self.num_actions
     temp_s_by_a = np.zeros((num_states, num_actions))
     for a in range(num_actions):
-      temp_s_by_a[:, a] = np.dot(self.P[a], self.r_bar)
+      temp_s_by_a[:, a] = np.dot(self.p[a], self.r_bar)
     self.r_bar = np.max(temp_s_by_a, axis=1)
     delta = np.zeros(num_states)
     for s in range(num_states):
       max_actions = np.where(temp_s_by_a[s] > self.r_bar[s] - self.threshold)[0]
-      temp_a = np.zeros(max_actions.__len__())
-      for i in range(max_actions.__len__()):
+      temp_a = np.zeros(len(max_actions))
+      for i in range(len(max_actions)):
         temp_a[i] = self.r[max_actions[i]][s] - self.r_bar[s] + np.dot(
-            self.P[max_actions[i]][s], self.v) - self.v[s]
+            self.p[max_actions[i]][s], self.v) - self.v[s]
       delta[s] = np.max(temp_a)
     self.v += self.alpha * delta
     self.r_bar += self.beta * delta
@@ -219,13 +220,13 @@ class MDVI_Control1(object):
     num_actions = self.num_actions
     temp_a = np.zeros(num_actions)
     for a in range(num_actions):
-      temp_a[a] = np.dot(self.P[a][idx], self.r_bar)
+      temp_a[a] = np.dot(self.p[a][idx], self.r_bar)
     self.r_bar[idx] = np.max(temp_a)
     max_actions = np.where(temp_a > self.r_bar[idx] - self.threshold)[0]
-    temp_a = np.zeros(max_actions.__len__())
-    for i in range(max_actions.__len__()):
+    temp_a = np.zeros(len(max_actions))
+    for i in range(len(max_actions)):
       temp_a[i] = self.r[max_actions[i]][idx] - self.r_bar[idx] + np.dot(
-          self.P[max_actions[i]][idx], self.v) - self.v[idx]
+          self.p[max_actions[i]][idx], self.v) - self.v[idx]
     delta = np.max(temp_a)
     self.v[idx] += self.alpha * delta
     self.r_bar[idx] += self.beta * delta
@@ -240,14 +241,14 @@ class MDVI_Control1(object):
 class MDVI_Control2(object):
   def __init__(self, mdp, init_v, init_r_bar, alpha=1.0, beta=1.0):
     # assert type(mdp) is MDP
-    self.P = mdp.P.copy()
-    self.r = mdp.r.copy()
+    self.p = mdp.transitions
+    self.r = mdp.rewards
     self.init_v = init_v.copy()
     self.init_r_bar = init_r_bar.copy()
     self.alpha = alpha
     self.beta = beta
-    self.num_states = mdp.num_states()
-    self.num_actions = mdp.num_actions()
+    self.num_states = mdp.num_states
+    self.num_actions = mdp.num_actions
     self.idx = 0
     self.reset()
 
@@ -256,11 +257,11 @@ class MDVI_Control2(object):
     num_actions = self.num_actions
     temp_s_by_a = np.zeros((num_states, num_actions))
     for a in range(num_actions):
-      temp_s_by_a[:, a] = np.dot(self.P[a], self.r_bar)
+      temp_s_by_a[:, a] = np.dot(self.p[a], self.r_bar)
     self.r_bar = np.max(temp_s_by_a, axis=1)
     temp_s_by_a = np.zeros((num_states, num_actions))
     for a in range(num_actions):
-      temp_s_by_a[:, a] = self.r[a] - self.r_bar + np.dot(self.P[a],
+      temp_s_by_a[:, a] = self.r[a] - self.r_bar + np.dot(self.p[a],
                                                           self.v) - self.v
     delta = np.max(temp_s_by_a, axis=1)
     self.v += self.alpha * delta
@@ -272,11 +273,11 @@ class MDVI_Control2(object):
     num_actions = self.num_actions
     temp_a = np.zeros(num_actions)
     for a in range(num_actions):
-      temp_a[a] = np.dot(self.P[a][idx], self.r_bar)
+      temp_a[a] = np.dot(self.p[a][idx], self.r_bar)
     self.r_bar[idx] = np.max(temp_a)
     temp_a = np.zeros(num_actions)
     for a in range(num_actions):
-      temp_a[a] = self.r[a][idx] - self.r_bar[idx] + np.dot(self.P[a][idx],
+      temp_a[a] = self.r[a][idx] - self.r_bar[idx] + np.dot(self.p[a][idx],
                                                             self.v) - self.v[
                     idx]
     delta = np.max(temp_a)
--- src/differential_value_iteration/environments/environments.py
+++ src/differential_value_iteration/environments/environments.py
@@ -3,78 +3,80 @@ import dataclasses
 import numpy as np
 
 
-@dataclasses.dataclass
-class MarkovRewardsProcess:
+@dataclasses.dataclass(frozen=True)
+class MarkovRewardProcess:
   # |S| x |S| array of state to state transition probabilities.
   transitions: np.ndarray
   # |S| vector of rewards for entering each state.
   rewards: np.ndarray
   name: str
 
+  def __post_init__(self):
+    """Raises error if transition probability do not sum to 1 for all states."""
+    state_probability_sums = self.transitions.sum(axis=-1)
+    failed_unity = np.where(state_probability_sums != 1., True, False)
+    num_invalid_states = np.sum(failed_unity)
+    if num_invalid_states:
+      bad_states = np.argwhere(failed_unity)
+      raise ValueError(
+          f'Invalid Reward Process, some states do not have transitions that sum to 1: {bad_states}')
+
   @property
   def num_states(self):
     return len(self.transitions)
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class MarkovDecisionProcess:
-  # |S| x |A| x |S| array of (state, action) -> state transition probabilities.
+  # |A| x |S| x |S| array of (state, action) -> state transition probabilities.
   transitions: np.ndarray
-  # |S| x |A| or |S| x |A| vector of rewards actions? entering or leaving?
+  # |A| or |S| vector of rewards for each action.
   rewards: np.ndarray
   name: str
 
-  @property
-  def num_states(self):
-    return len(self.transitions)
+  def __post_init__(self):
+    """Raises error if transition probability do not sum to 1 for all states."""
+    for action_idx, transitions in enumerate(self.transitions):
+      state_probability_sums = transitions.sum(axis=-1)
+      failed_unity = np.where(state_probability_sums != 1., True, False)
+      num_invalid_states = np.sum(failed_unity)
+      if num_invalid_states:
+        bad_states = np.argwhere(failed_unity)
+        raise ValueError(
+            f'Invalid Decision Process, action:{action_idx}, some states do not have transitions that sum to 1: {bad_states}')
 
   @property
-  def num_actions(self):
+  def num_states(self):
     return self.transitions.shape[1]
 
-
-class MDP_old:
-  def __init__(self, P, r, name):
-    self.P = P
-    self.r = r
-    self.name = name
-
-  def is_multichain(self):
-    raise NotImplementedError
-
-  def num_states(self):
-    return self.P.shape[0]
-
+  @property
   def num_actions(self):
-    return self.P.shape[1]
+    return len(self.transitions)
 
 
-mrp1 = MarkovRewardsProcess(
+mrp1 = MarkovRewardProcess(
     transitions=np.array([
         [0, 1, 0],
         [0, 0, 1],
         [1, 0, 0]
     ], dtype=np.float32), rewards=np.array([0, 0, 3], dtype=np.float32),
     name='mrp1')
-#
-# mrp1 = MRP(np.array([
-#     [0, 1, 0],
-#     [0, 0, 1],
-#     [1, 0, 0]
-# ], dtype=float), np.array([0, 0, 3], dtype=float), 'mrp1'
-# )
-#
-# mrp2 = MRP(np.array([
-#     [0.0, 0.9, 0.1, 0.0],
-#     [0.1, 0.0, 0.9, 0.0],
-#     [0.9, 0.1, 0.0, 0.0],
-#     [0.0, 0.0, 0.0, 1.0]
-# ]), np.array([0.0, 1.0, 8.0, 20.0]), 'mrp2')
-#
-# mrp3 = MRP(np.array([
-#     [0.2, 0.8],
-#     [0.2, 0.8]
-# ]), np.array([1.0, 1.0]), 'mrp3')
+
+mrp2 = MarkovRewardProcess(
+    transitions=np.array([
+        [0, .9, .1, 0],
+        [.1, 0, .9, 0],
+        [.9, .1, 0, 0],
+        [0, 0, 0, 1]], dtype=np.float32),
+    rewards=np.array([0, 1, 8, 20], dtype=np.float32),
+    name='mrp2')
+
+mrp3 = MarkovRewardProcess(
+    transitions=np.array([
+        [.2, .8],
+        [.2, .8]], dtype=np.float32),
+    rewards=np.array([1, 1], dtype=np.float32),
+    name='mrp3')
 
 mdp1 = MarkovDecisionProcess(transitions=np.array([
     [[1, 0], [0, 1]],  # first action
@@ -91,20 +93,3 @@ mdp2 = MarkovDecisionProcess(transitions=np.array([
     [1, 1],  # first action
     [0, 2]  # second action
 ], dtype=np.float32), name='mdp2')
-
-#
-# mdp1 = MDP(np.array([
-#     [[1.0, 0.0], [0.0, 1.0]],  # first action
-#     [[0.0, 1.0], [1.0, 0.0]]  # second action
-# ]), np.array([
-#     [1.0, 1.0],  # first action
-#     [0.0, 0.0]  # second action
-# ]), 'mdp1')
-#
-# mdp2 = MDP(np.array([
-#     [[1.0, 0.0], [0.0, 1.0]],  # first action
-#     [[1.0, 0.0], [0.0, 1.0]]  # second action
-# ]), np.array([
-#     [1.0, 1.0],  # first action
-#     [1.0, 2.0]  # second action
-# ]), 'mdp2')
