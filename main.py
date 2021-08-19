ff28a89013b39d0707894a099333a910929a4b50
Brian Tanner <tanno@google.com>
2021-08-19 10:59:04 -0600

    Changed utils import to be module, not individual functions (https://google.github.io/styleguide/pyguide.html#22-imports).

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 from pathlib import Path
 
 import numpy as np
+from differential_value_iteration import utils
 from differential_value_iteration.algorithms import algorithms
 from differential_value_iteration.environments import environments
-from differential_value_iteration.utils import draw
-from differential_value_iteration.utils import run_alg
 
 
 def run():
@@ -24,23 +23,26 @@ def run():
     init_r_bar_vec = np.zeros(env.num_states)
     results = exp_RVI_Evaluation(env, 'exec_sync', alphas, init_v, max_iters,
                                  epsilon, ref_idx=0)
-    draw(results, plots_dir + env.name + '_RVI_Evaluation_sync', alphas)
+    utils.draw(results, plots_dir + env.name + '_RVI_Evaluation_sync', alphas)
     results = exp_RVI_Evaluation(env, 'exec_async', alphas, init_v, max_iters,
                                  epsilon, ref_idx=0)
-    draw(results, plots_dir + env.name + '_RVI_Evaluation_async', alphas)
+    utils.draw(results, plots_dir + env.name + '_RVI_Evaluation_async', alphas)
     results = exp_DVI_Evaluation(env, 'exec_sync', alphas, betas, init_v,
                                  init_r_bar_scalar, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_DVI_Evaluation_sync', alphas, betas)
+    utils.draw(results, plots_dir + env.name + '_DVI_Evaluation_sync', alphas,
+               betas)
     results = exp_DVI_Evaluation(env, 'exec_async', alphas, betas, init_v,
                                  init_r_bar_scalar, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_DVI_Evaluation_async', alphas, betas)
+    utils.draw(results, plots_dir + env.name + '_DVI_Evaluation_async', alphas,
+               betas)
     results = exp_MDVI_Evaluation(env, 'exec_sync', alphas, betas, init_v,
                                   init_r_bar_vec, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_MDVI_Evaluation_sync', alphas, betas)
+    utils.draw(results, plots_dir + env.name + '_MDVI_Evaluation_sync', alphas,
+               betas)
     results = exp_MDVI_Evaluation(env, 'exec_async', alphas, betas, init_v,
                                   init_r_bar_vec, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_MDVI_Evaluation_async', alphas,
-         betas)
+    utils.draw(results, plots_dir + env.name + '_MDVI_Evaluation_async', alphas,
+               betas)
 
   envs = [environments.mdp2]
   for env in envs:
@@ -49,28 +51,34 @@ def run():
     init_r_bar_vec = np.zeros(env.num_states)
     results = exp_RVI_Control(env, 'exec_sync', alphas, init_v, max_iters,
                               epsilon, ref_idx=0)
-    draw(results, plots_dir + env.name + '_RVI_Control_sync', alphas)
+    utils.draw(results, plots_dir + env.name + '_RVI_Control_sync', alphas)
     results = exp_RVI_Control(env, 'exec_async', alphas, init_v, max_iters,
                               epsilon, ref_idx=0)
-    draw(results, plots_dir + env.name + '_RVI_Control_async', alphas)
+    utils.draw(results, plots_dir + env.name + '_RVI_Control_async', alphas)
     results = exp_DVI_Control(env, 'exec_sync', alphas, betas, init_v,
                               init_r_bar_scalar, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_DVI_Control_sync', alphas, betas)
+    utils.draw(results, plots_dir + env.name + '_DVI_Control_sync', alphas,
+               betas)
     results = exp_DVI_Control(env, 'exec_async', alphas, betas, init_v,
                               init_r_bar_scalar, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_DVI_Control_async', alphas, betas)
+    utils.draw(results, plots_dir + env.name + '_DVI_Control_async', alphas,
+               betas)
     results = exp_MDVI_Control1(env, 'exec_sync', alphas, betas, init_v,
                                 init_r_bar_vec, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_MDVI_Control1_sync', alphas, betas)
+    utils.draw(results, plots_dir + env.name + '_MDVI_Control1_sync', alphas,
+               betas)
     results = exp_MDVI_Control1(env, 'exec_async', alphas, betas, init_v,
                                 init_r_bar_vec, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_MDVI_Control1_async', alphas, betas)
+    utils.draw(results, plots_dir + env.name + '_MDVI_Control1_async', alphas,
+               betas)
     results = exp_MDVI_Control2(env, 'exec_sync', alphas, betas, init_v,
                                 init_r_bar_vec, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_MDVI_Control2_sync', alphas, betas)
+    utils.draw(results, plots_dir + env.name + '_MDVI_Control2_sync', alphas,
+               betas)
     results = exp_MDVI_Control2(env, 'exec_async', alphas, betas, init_v,
                                 init_r_bar_vec, max_iters, epsilon)
-    draw(results, plots_dir + env.name + '_MDVI_Control2_async', alphas, betas)
+    utils.draw(results, plots_dir + env.name + '_MDVI_Control2_async', alphas,
+               betas)
 
 
 def exp_RVI_Evaluation(env, update_rule, alphas, init_v, max_iters, epsilon,
@@ -79,7 +87,7 @@ def exp_RVI_Evaluation(env, update_rule, alphas, init_v, max_iters, epsilon,
   for alpha_idx, alpha in enumerate(alphas):
     alg = algorithms.RVI_Evaluation(env, init_v, alpha, ref_idx)
     print(f'{env.name} RVI Evaluation {update_rule} alpha:{alpha}', end=' ')
-    convergence = run_alg(alg, update_rule, max_iters, epsilon)
+    convergence = utils.run_alg(alg, update_rule, max_iters, epsilon)
     print(f'Converged? {convergence}')
     convergence_flags[alpha_idx] = convergence
   return convergence_flags
@@ -91,7 +99,7 @@ def exp_RVI_Control(env, update_rule, alphas, init_v, max_iters, epsilon,
   for alpha_idx, alpha in enumerate(alphas):
     alg = algorithms.RVI_Control(env, init_v, alpha, ref_idx)
     print(f'{env.name} RVI Control {update_rule} alpha:{alpha}', end=' ')
-    convergence = run_alg(alg, update_rule, max_iters, epsilon)
+    convergence = utils.run_alg(alg, update_rule, max_iters, epsilon)
     print(f'Converged? {convergence}')
     convergence_flags[alpha_idx] = convergence
   return convergence_flags
@@ -106,7 +114,7 @@ def exp_DVI_Evaluation(env, update_rule, alphas, betas, init_v, init_r_bar,
       print(
           f'{env.name} DVI Evaluation {update_rule} alpha:{alpha} beta:{beta}',
           end=' ')
-      convergence = run_alg(alg, update_rule, max_iters, epsilon)
+      convergence = utils.run_alg(alg, update_rule, max_iters, epsilon)
       print(f'Converged? {convergence}')
       convergence_flags[alpha_idx, beta_idx] = convergence
   return convergence_flags
@@ -120,7 +128,7 @@ def exp_DVI_Control(env, update_rule, alphas, betas, init_v, init_r_bar,
       alg = algorithms.DVI_Control(env, init_v, init_r_bar, alpha, beta)
       print(f'{env.name} DVI Control {update_rule} alpha:{alpha} beta:{beta}',
             end=' ')
-      convergence = run_alg(alg, update_rule, max_iters, epsilon)
+      convergence = utils.run_alg(alg, update_rule, max_iters, epsilon)
       print(f'Converged? {convergence}')
       convergence_flags[alpha_idx, beta_idx] = convergence
   return convergence_flags
@@ -135,7 +143,7 @@ def exp_MDVI_Evaluation(env, update_rule, alphas, betas, init_v, init_r_bar,
       print(
           f'{env.name} MDVI Evaluation {update_rule} alpha:{alpha} beta:{beta}',
           end=' ')
-      convergence = run_alg(alg, update_rule, max_iters, epsilon)
+      convergence = utils.run_alg(alg, update_rule, max_iters, epsilon)
       print(f'Converged? {convergence}')
       convergence_flags[alpha_idx, beta_idx] = convergence
   return convergence_flags
@@ -149,7 +157,7 @@ def exp_MDVI_Control1(env, update_rule, alphas, betas, init_v, init_r_bar,
       alg = algorithms.MDVI_Control1(env, init_v, init_r_bar, alpha, beta)
       print(f'{env.name} MDVI Control1 {update_rule} alpha:{alpha} beta:{beta}',
             end=' ')
-      convergence = run_alg(alg, update_rule, max_iters, epsilon)
+      convergence = utils.run_alg(alg, update_rule, max_iters, epsilon)
       print(f'Converged? {convergence}')
       convergence_flags[alpha_idx, beta_idx] = convergence
   return convergence_flags
@@ -163,7 +171,7 @@ def exp_MDVI_Control2(env, update_rule, alphas, betas, init_v, init_r_bar,
       alg = algorithms.MDVI_Control2(env, init_v, init_r_bar, alpha, beta)
       print(f'{env.name} MDVI Control2 {update_rule} alpha:{alpha} beta:{beta}',
             end=' ')
-      convergence = run_alg(alg, update_rule, max_iters, epsilon)
+      convergence = utils.run_alg(alg, update_rule, max_iters, epsilon)
       print(f'Converged? {convergence}')
       convergence_flags[alpha_idx, beta_idx] = convergence
   return convergence_flags
