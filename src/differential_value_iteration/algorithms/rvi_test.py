0c2e836c14bc8c5b18e43944b1f71f3a4e63f616
Brian Tanner <tanno@google.com>
2021-09-08 18:37:13 -0600

    Added ability to create MRPs that are float32 or float64 datatypes. Updated tests to use both, with tuned tolerances for each. Proof of concept that our convergence changes depended on numerical precision.

--- src/differential_value_iteration/algorithms/rvi_test.py
+++ src/differential_value_iteration/algorithms/rvi_test.py
@@ -1,31 +1,37 @@
 """Tests for basic functioning of RVI algorithms."""
 import numpy as np
 from absl.testing import absltest
+from absl.testing import parameterized
+
 from differential_value_iteration.algorithms import rvi
 from differential_value_iteration.environments import micro
 
 
-class RVITest(absltest.TestCase):
+class RVITest(parameterized.TestCase):
 
-  def test_rvi_sync_converges(self):
-    environment = micro.mrp1
+  @parameterized.parameters(np.float32, np.float64)
+  def test_rvi_sync_converges(self, dtype: np.dtype):
+    tolerance_places = 6 if dtype is np.float32 else 10
+    environment = micro.create_mrp1(dtype)
     algorithm = rvi.Evaluation(
         mrp=environment,
         step_size=.5,
-        initial_values=np.zeros(environment.num_states, dtype=np.float32),
+        initial_values=np.zeros(environment.num_states, dtype=dtype),
         reference_index=0,
         synchronized=True)
 
     for _ in range(50):
       changes = algorithm.update()
-    self.assertAlmostEqual(np.sum(np.abs(changes)), 0., places=7)
+    self.assertAlmostEqual(np.sum(np.abs(changes)), 0., places=tolerance_places)
 
-  def test_rvi_async_converges(self):
-    environment = micro.mrp1
+  @parameterized.parameters(np.float32, np.float64)
+  def test_rvi_async_converges(self, dtype: np.dtype):
+    tolerance_places = 6 if dtype is np.float32 else 10
+    environment = micro.create_mrp1(dtype)
     algorithm = rvi.Evaluation(
         mrp=environment,
         step_size=.5,
-        initial_values=np.zeros(environment.num_states, dtype=np.float32),
+        initial_values=np.zeros(environment.num_states, dtype=dtype),
         reference_index=0,
         synchronized=False)
 
@@ -34,7 +40,7 @@ class RVITest(absltest.TestCase):
       for _ in range(environment.num_states):
         change = algorithm.update()
         change_sum += np.abs(change)
-    self.assertAlmostEqual(change_sum, 0., places=7)
+    self.assertAlmostEqual(change_sum, 0., places=tolerance_places)
 
 
 if __name__ == '__main__':
