fd81bc0a4eab922473d2fa5979626dde94a19fc9
Brian Tanner <tanno@google.com>
2021-09-10 13:44:14 -0600

    Adds control version of dvi and tests on MDP1.

--- src/differential_value_iteration/algorithms/dvi.py
+++ src/differential_value_iteration/algorithms/dvi.py
@@ -7,7 +7,8 @@ from differential_value_iteration.environments import structure
 
 
 class Evaluation(algorithm.Evaluation):
-  """Differential Value Iteration for prediction, section 2.1.1 in paper."""
+  """Differential Value Iteration for prediction."""
+
   def __init__(
       self,
       mrp: structure.MarkovRewardProcess,
@@ -48,18 +49,92 @@ class Evaluation(algorithm.Evaluation):
       return self.update_sync()
     return self.update_async()
 
-  def update_sync(self):
+  def update_sync(self) -> np.ndarray:
     changes = self.mrp.rewards - self.r_bar + np.dot(self.mrp.transitions,
                                                      self.current_values) - self.current_values
     self.current_values += self.step_size * changes
     self.r_bar += self.beta * np.sum(changes)
     return changes
 
-  def update_async(self):
+  def update_async(self) -> np.ndarray:
     change = self.mrp.rewards[self.index] - self.r_bar + np.dot(
         self.mrp.transitions[self.index],
         self.current_values) - self.current_values[self.index]
     self.current_values[self.index] += self.step_size * change
     self.r_bar += self.beta * change
     self.index = (self.index + 1) % self.mrp.num_states
+    return change
+
+
+class Control(algorithm.Evaluation):
+  """Differential Value Iteration for control."""
+
+  def __init__(
+      self,
+      mdp: structure.MarkovDecisionProcess,
+      initial_values: np.ndarray,
+      initial_r_bar: float,
+      step_size: float,
+      beta: float,
+      synchronized: bool):
+    self.mdp = mdp
+    # Ensure internal value types match environment precision.
+    self.initial_values = initial_values.copy().astype(mdp.rewards.dtype)
+    self.initial_r_bar = mdp.rewards.dtype.type(initial_r_bar)
+    self.step_size = mdp.rewards.dtype.type(step_size)
+    self.beta = mdp.rewards.dtype.type(beta)
+
+    self.index = 0
+    self.synchronized = synchronized
+    self.reset()
+
+  def reset(self):
+    self.current_values = self.initial_values.copy()
+    self.r_bar = self.initial_r_bar
+
+  def diverged(self) -> bool:
+    if not np.isfinite(self.current_values).all():
+      logging.warn('Current values not finite in DVI.')
+      return True
+    if not np.isfinite(self.r_bar):
+      logging.warn('r_bar not finite in DVI.')
+      return True
+    return False
+
+  def types_ok(self) -> bool:
+    return self.r_bar.dtype == self.mdp.rewards.dtype and self.current_values.dtype == self.mdp.rewards.dtype
+
+  def update(self) -> np.ndarray:
+    if self.synchronized:
+      return self.update_sync()
+    return self.update_async()
+
+  def update_sync(self) -> np.ndarray:
+    # temp_s_by_a = np.zeros((self.num_states, self.num_actions))
+    # for a in range(self.num_actions):
+    #   temp_s_by_a[:, a] = self.r[a] - self.r_bar + np.dot(self.p[a],
+    #                                                       self.v) - self.v
+    temp_s_by_a = self.mdp.rewards - self.r_bar + np.dot(self.mdp.transitions,
+                                                         self.current_values) - self.current_values
+    changes = np.max(temp_s_by_a, axis=0)
+    self.current_values += self.step_size * changes
+    self.r_bar += self.beta * np.sum(changes)
+
+    return changes
+
+  def update_async(self) -> np.ndarray:
+    # temp_a = np.zeros(self.num_actions)
+    # for a in range(self.num_actions):
+    #   temp_a[a] = self.r[a][idx] - self.r_bar + np.dot(self.p[a][idx], self.v) - \
+    #               self.v[idx]
+    # delta = np.max(temp_a)
+    # self.v[idx] += self.alpha * delta
+    # self.r_bar += self.beta * delta
+    temp_a = self.mdp.rewards[:, self.index] - self.r_bar + np.dot(
+        self.mdp.transitions[:, self.index], self.current_values) - \
+             self.current_values[self.index]
+    change = np.max(temp_a)
+    self.current_values[self.index] += self.step_size * change
+    self.r_bar += self.beta * change
+    self.index = (self.index + 1) % self.mdp.num_states
     return change
--- src/differential_value_iteration/algorithms/dvi_test.py
+++ src/differential_value_iteration/algorithms/dvi_test.py
@@ -1,12 +1,16 @@
 """Tests for basic functioning of DVI algorithms."""
+import itertools
+from typing import Callable
+
 import numpy as np
 from absl.testing import absltest
 from absl.testing import parameterized
 from differential_value_iteration.algorithms import dvi
 from differential_value_iteration.environments import micro
+from differential_value_iteration.environments import structure
 
 
-class DVITest(parameterized.TestCase):
+class DVIEvaluationTest(parameterized.TestCase):
 
   @parameterized.parameters(np.float32, np.float64)
   def test_dvi_sync_converges(self, dtype: np.dtype):
@@ -37,6 +41,67 @@ class DVITest(parameterized.TestCase):
     environment = micro.create_mrp1(dtype)
     algorithm = dvi.Evaluation(
         mrp=environment,
+        step_size=.5,
+        beta=.5,
+        initial_r_bar=.5,
+        initial_values=np.zeros(environment.num_states, dtype=dtype),
+        synchronized=False)
+
+    for _ in range(50):
+      change_sum = 0.
+      for _ in range(environment.num_states):
+        change = algorithm.update()
+        change_sum += np.abs(change)
+
+    with self.subTest('did_not_diverge'):
+      self.assertFalse(algorithm.diverged())
+    with self.subTest('maintained_types'):
+      self.assertTrue(algorithm.types_ok())
+    with self.subTest('converged'):
+      self.assertAlmostEqual(change_sum, 0., places=tolerance_places)
+
+
+class DVIControlTest(parameterized.TestCase):
+
+  @parameterized.parameters(itertools.product(
+      (micro.create_mdp1,),
+      (np.float32, np.float64))
+  )
+  def test_dvi_sync_converges(self,
+      mdp_constructor: Callable[[np.dtype], structure.MarkovDecisionProcess],
+      dtype: np.dtype):
+    tolerance_places = 10
+    environment = mdp_constructor(dtype)
+    algorithm = dvi.Control(
+        mdp=environment,
+        step_size=.5,
+        beta=.5,
+        initial_r_bar=.5,
+        initial_values=np.zeros(environment.num_states, dtype=dtype),
+        synchronized=True)
+
+    for _ in range(50):
+      changes = algorithm.update()
+
+    with self.subTest('did_not_diverge'):
+      self.assertFalse(algorithm.diverged())
+    with self.subTest('maintained_types'):
+      self.assertTrue(algorithm.types_ok())
+    with self.subTest('converged'):
+      self.assertAlmostEqual(np.sum(np.abs(changes)), 0.,
+                             places=tolerance_places)
+
+  @parameterized.parameters(itertools.product(
+      (micro.create_mdp1,),
+      (np.float32, np.float64))
+  )
+  def test_dvi_async_converges(self,
+      mdp_constructor: Callable[[np.dtype], structure.MarkovDecisionProcess],
+      dtype: np.dtype):
+    tolerance_places = 10
+    environment = mdp_constructor(dtype)
+    algorithm = dvi.Control(
+        mdp=environment,
         step_size=.5,
         beta=.5,
         initial_r_bar=.5,
