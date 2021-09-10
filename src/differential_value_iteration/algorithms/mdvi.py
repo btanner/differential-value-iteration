eb008aa8ab8712fc65cfb6ccf3666f720a211b86
Brian Tanner <tanno@google.com>
2021-09-10 14:48:21 -0600

    Adds MDVI Control 1, and tests. Converges on MDP1 and MDP2, async and sync.

--- src/differential_value_iteration/algorithms/mdvi.py
+++ src/differential_value_iteration/algorithms/mdvi.py
@@ -56,7 +56,7 @@ class Evaluation(algorithm.Evaluation):
       return self.update_sync()
     return self.update_async()
 
-  def update_sync(self):
+  def update_sync(self) -> np.ndarray:
     self.r_bar = np.dot(self.mrp.transitions, self.r_bar)
     changes = self.mrp.rewards - self.r_bar + np.dot(self.mrp.transitions,
                                                      self.current_values) - self.current_values
@@ -64,7 +64,7 @@ class Evaluation(algorithm.Evaluation):
     self.r_bar += self.beta * changes
     return changes
 
-  def update_async(self):
+  def update_async(self) -> np.ndarray:
     self.r_bar[self.index] = np.dot(self.mrp.transitions[self.index],
                                     self.r_bar)
     change = self.mrp.rewards[self.index] - self.r_bar[self.index] + np.dot(
@@ -74,3 +74,105 @@ class Evaluation(algorithm.Evaluation):
     self.r_bar[self.index] += self.beta * change
     self.index = (self.index + 1) % self.mrp.num_states
     return change
+
+
+class Control(algorithm.Evaluation):
+  """Multichain DVI for prediction, section 3.1.1 in paper."""
+
+  def __init__(
+      self,
+      mdp: structure.MarkovDecisionProcess,
+      initial_values: np.ndarray,
+      initial_r_bar: Union[float, np.ndarray],
+      step_size: float,
+      beta: float,
+      threshold: float,
+      synchronized: bool):
+    self.mdp = mdp
+    # Ensure internal value types match environment precision.
+    self.initial_values = initial_values.copy().astype(mdp.rewards.dtype)
+    if isinstance(initial_r_bar, np.ndarray):
+      self.initial_r_bar = initial_r_bar.copy().astype(mdp.rewards.dtype)
+    else:
+      self.initial_r_bar = np.full(shape=mdp.num_states,
+                                   fill_value=initial_r_bar,
+                                   dtype=mdp.rewards.dtype)
+    self.step_size = mdp.rewards.dtype.type(step_size)
+    self.beta = mdp.rewards.dtype.type(beta)
+    self.threshold = mdp.rewards.dtype.type(threshold)
+    self.index = 0
+    self.synchronized = synchronized
+    self.current_values = None
+    self.r_bar = None
+    self.reset()
+
+  def reset(self):
+    self.current_values = self.initial_values.copy()
+    self.r_bar = self.initial_r_bar.copy()
+
+  def diverged(self) -> bool:
+    if not np.isfinite(self.current_values).all():
+      logging.warn('Current values not finite in MDVI.')
+      return True
+    if not np.isfinite(self.r_bar).all():
+      logging.warn('r_bar not finite in MDVI.')
+      return True
+    return False
+
+  def types_ok(self) -> bool:
+    if self.r_bar.dtype != self.mdp.rewards.dtype:
+      print(f'r_bar: {self.r_bar.dtype} vs {self.mdp.rewards.dtype}')
+    if self.current_values.dtype != self.mdp.rewards.dtype:
+      print(f'current_values: {self.current_values.dtype} vs {self.mdp.rewards.dtype}')
+    return self.r_bar.dtype == self.mdp.rewards.dtype and self.current_values.dtype == self.mdp.rewards.dtype
+
+  def update(self) -> np.ndarray:
+    if self.synchronized:
+      return self.update_sync()
+    return self.update_async()
+
+  def update_sync(self) -> np.ndarray:
+    temp_s_by_a = np.zeros((self.mdp.num_states, self.mdp.num_actions), dtype=self.mdp.rewards.dtype)
+    for a in range(self.mdp.num_actions):
+      temp_s_by_a[:, a] = np.dot(self.mdp.transitions[a], self.r_bar)
+    self.r_bar = np.max(temp_s_by_a, axis=1)
+    delta = np.zeros(self.mdp.num_states, dtype=self.mdp.rewards.dtype)
+    for s in range(self.mdp.num_states):
+      max_actions = np.where(temp_s_by_a[s] > self.r_bar[s] - self.threshold)[0]
+      temp_a = np.zeros(len(max_actions), dtype=self.mdp.rewards.dtype)
+      for i in range(len(max_actions)):
+        temp_a[i] = self.mdp.rewards[max_actions[i]][s] - self.r_bar[s] + np.dot(
+            self.mdp.transitions[max_actions[i]][s], self.current_values) - self.current_values[s]
+      delta[s] = np.max(temp_a)
+    self.current_values += self.step_size * delta
+    self.r_bar += self.beta * delta
+    return delta
+
+  def update_async(self) -> np.ndarray:
+    temp_a = np.zeros(self.mdp.num_actions, self.mdp.rewards.dtype)
+    for a in range(self.mdp.num_actions):
+      temp_a[a] = np.dot(self.mdp.transitions[a][self.index], self.r_bar)
+    self.r_bar[self.index] = np.max(temp_a)
+    max_actions = np.where(temp_a > self.r_bar[self.index] - self.threshold)[0]
+    temp_a = np.zeros(len(max_actions), dtype=self.mdp.rewards.dtype)
+    for i in range(len(max_actions)):
+      temp_a[i] = self.mdp.rewards[max_actions[i]][self.index] - self.r_bar[self.index] + np.dot(
+          self.mdp.transitions[max_actions[i]][self.index], self.current_values) - self.current_values[self.index]
+    delta = np.max(temp_a)
+    self.current_values[self.index] += self.step_size * delta
+    self.r_bar[self.index] += self.beta * delta
+    self.index = (self.index + 1) % self.mdp.num_states
+    return delta
+
+
+
+
+  # self.r_bar[self.index] = np.dot(self.mrp.transitions[self.index],
+    #                                 self.r_bar)
+    # change = self.mrp.rewards[self.index] - self.r_bar[self.index] + np.dot(
+    #     self.mrp.transitions[self.index],
+    #     self.current_values) - self.current_values[self.index]
+    # self.current_values[self.index] += self.step_size * change
+    # self.r_bar[self.index] += self.beta * change
+    # self.index = (self.index + 1) % self.mrp.num_states
+    # return change
--- src/differential_value_iteration/algorithms/mdvi_test.py
+++ src/differential_value_iteration/algorithms/mdvi_test.py
@@ -10,7 +10,7 @@ from differential_value_iteration.environments import micro
 from differential_value_iteration.environments import structure
 
 
-class MDVITest(parameterized.TestCase):
+class MDVIEvaluationTest(parameterized.TestCase):
 
   @parameterized.parameters(itertools.product(
       (micro.create_mrp1, micro.create_mrp2),
@@ -55,7 +55,7 @@ class MDVITest(parameterized.TestCase):
     if dtype == np.float64:
       tolerance_places = 8
     else:
-      tolerance_places = 4 if env_constructor == micro.create_mrp2 else 6
+      tolerance_places = 4 if mrp_constructor == micro.create_mrp2 else 6
     environment = mrp_constructor(dtype)
     initial_r_bar = 0. if r_bar_scalar else np.full(environment.num_states,
                                                     0., dtype)
@@ -81,5 +81,77 @@ class MDVITest(parameterized.TestCase):
       self.assertAlmostEqual(change_sum, 0., places=tolerance_places)
 
 
+
+class MDVIControlTest(parameterized.TestCase):
+
+  @parameterized.parameters(itertools.product(
+      (micro.create_mdp1, micro.create_mdp2),
+      (False, True),
+      (np.float32, np.float64)))
+  def test_mdvi_sync_converges(self,
+      mdp_constructor: Callable[[np.dtype], structure.MarkovDecisionProcess],
+      r_bar_scalar: bool, dtype: np.dtype):
+    tolerance_places = 8 if dtype == np.float64 else 5
+    environment = mdp_constructor(dtype)
+    initial_r_bar = 0. if r_bar_scalar else np.full(environment.num_states,
+                                                    0., dtype)
+    algorithm = mdvi.Control(
+        mdp=environment,
+        step_size=.1,
+        beta=.1,
+        threshold=.01,
+        initial_r_bar=initial_r_bar,
+        initial_values=np.zeros(environment.num_states, dtype=dtype),
+        synchronized=True)
+
+    with self.subTest('initial_types'):
+      self.assertTrue(algorithm.types_ok())
+
+    for _ in range(250):
+      changes = algorithm.update()
+
+    with self.subTest('did_not_diverge'):
+      self.assertFalse(algorithm.diverged())
+    with self.subTest('maintained_types'):
+      self.assertTrue(algorithm.types_ok())
+    with self.subTest('converged'):
+      self.assertAlmostEqual(np.sum(np.abs(changes)), 0.,
+                             places=tolerance_places)
+
+  @parameterized.parameters(itertools.product(
+      (micro.create_mdp1, micro.create_mdp2),
+      (False, True),
+      (np.float32, np.float64)))
+  def test_mdvi_async_converges(self,
+      mdp_constructor: Callable[[np.dtype], structure.MarkovDecisionProcess],
+      r_bar_scalar: bool, dtype: np.dtype):
+    tolerance_places = 8 if dtype == np.float64 else 5
+    environment = mdp_constructor(dtype)
+    initial_r_bar = 0. if r_bar_scalar else np.full(environment.num_states,
+                                                    0., dtype)
+    algorithm = mdvi.Control(
+        mdp=environment,
+        step_size=.1,
+        beta=.1,
+        threshold=.01,
+        initial_r_bar=initial_r_bar,
+        initial_values=np.zeros(environment.num_states, dtype=dtype),
+        synchronized=False)
+
+    with self.subTest('initial_types'):
+      self.assertTrue(algorithm.types_ok())
+
+    for _ in range(250):
+      change_sum = 0.
+      for _ in range(environment.num_states):
+        change = algorithm.update()
+        change_sum += np.abs(change)
+
+    with self.subTest('did_not_diverge'):
+      self.assertFalse(algorithm.diverged())
+    with self.subTest('maintained_types'):
+      self.assertTrue(algorithm.types_ok())
+    with self.subTest('converged'):
+      self.assertAlmostEqual(change_sum, 0., places=tolerance_places)
 if __name__ == '__main__':
   absltest.main()
