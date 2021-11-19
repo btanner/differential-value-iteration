5f5605fcd0a02c8f60a91068fd7f726ef64bac39
Brian Tanner <tanno@google.com>
2021-11-18 17:04:38 -0700

    Adds MM1 Queue and updates benchmark to support it.

--- src/differential_value_iteration/experiments/control_benchmark.py
+++ src/differential_value_iteration/experiments/control_benchmark.py
@@ -14,6 +14,7 @@ from differential_value_iteration.algorithms import mdvi
 from differential_value_iteration.algorithms import rvi
 from differential_value_iteration.environments import garet
 from differential_value_iteration.environments import micro
+from differential_value_iteration.environments import mm1_queue
 from differential_value_iteration.environments import structure
 
 FLAGS = flags.FLAGS
@@ -21,8 +22,8 @@ _NUM_ITERS = flags.DEFINE_integer('num_iters', 1000,
                                   'Number of iterations per algorithm.')
 _SYNCHRONIZED = flags.DEFINE_bool('synchronized', True,
                                   'Run algorithms in synchronized mode.')
-_64bit = flags.DEFINE_bool('64bit', False,
-                           'Use 64 bit precision (default is 32 bit).')
+_32bit = flags.DEFINE_bool('32bit', False,
+                           'Use 32 bit precision (default is 64 bit).')
 
 _CONVERGENCE_TOLERANCE = flags.DEFINE_float('convergence_tolerance', 1e-5,
                                             'Tolerance for convergence.')
@@ -39,6 +40,7 @@ _GARET2 = flags.DEFINE_bool('garet2', True, 'Include GARET 2 in benchmark.')
 _GARET3 = flags.DEFINE_bool('garet3', True, 'Include GARET 3 in benchmark.')
 _GARET_100 = flags.DEFINE_bool('garet_100', True,
                                'Include GARET 100 in benchmark.')
+_MM1_1 = flags.DEFINE_bool('MM1_1', True, 'Include MM1 Queue 1 in benchmark.')
 
 
 def run(
@@ -59,6 +61,7 @@ def run(
       """
   for environment in environments:
     print(f'\nEnvironment: {environment.name}\n----------------------')
+    # print('\n', environment.rewards)
     initial_values = np.zeros(environment.num_states)
     inner_loop_range = 1 if synchronized else environment.num_states
     for algorithm_constructor in algorithm_constructors:
@@ -89,11 +92,41 @@ def run(
 
         if change_summary <= convergence_tolerance and i > 1:
           converged = True
+          break
       converged_string = 'YES\t' if converged else 'NO\t'
+      mean_return, std_return = estimate_policy_average_reward(alg, environment)
+
       if diverged:
         converged_string = 'DIVERGED'
       print(
-        f'Average Time:{1000.*total_time/i:.3f} ms\tConverged:{converged_string}\t{i} iterations\tMean final Change:{np.mean(np.abs(changes)):.5f}')
+        f'Average Time:{1000.*total_time/i:.3f} ms\tConverged:{converged_string}\t{i} iters\tMean final Change:{np.mean(np.abs(changes)):.5f} Avg Reward: {mean_return:.2f} ({std_return:.2f})')
+      print(f'Policy: {alg.greedy_policy()}')
+      # print(f'Estimates: {alg.get_estimates()}')
+
+def estimate_policy_average_reward(alg, environment):
+  policy = alg.greedy_policy()
+  mc = environment.as_markov_chain_from_deterministic_policy(policy)
+  sim_length = 1000
+  num_reps = 100
+  starting_state = 0
+  returns = np.zeros(num_reps, dtype=np.float64)
+
+  # Generatate sequences of states under greedy policy.
+  simulation_results = mc.simulate(ts_length=sim_length,
+                                   init=starting_state,
+                                   num_reps=num_reps)
+  for i, states_visited in enumerate(simulation_results):
+    if max(states_visited) > len(policy):
+      print(f'apparently visited states: {states_visited} but have policy: {policy}')
+    # If this crashes, then mc.simulate is sampling invalid states b/c CDFS don't sum to 1.
+    # In that case, check if any states_visited are == num_states, and if so, resample.
+    actions_taken = policy[states_visited]
+    rewards_seen = [environment.rewards[a, s] for a, s in zip (actions_taken, states_visited)]
+    returns[i] = np.mean(rewards_seen)
+  return np.mean(returns), np.std(returns)
+
+
+
 
 
 def main(argv):
@@ -104,23 +137,23 @@ def main(argv):
   # Create constructors that only depends on params common to all algorithms.
   if FLAGS.dvi:
     dvi_algorithm = functools.partial(dvi.Control,
-                                      step_size=.1,
-                                      beta=.1,
+                                      step_size=1.,
+                                      beta=1.,
                                       initial_r_bar=0.)
     algorithm_constructors.append(dvi_algorithm)
 
   if FLAGS.mdvi:
     mdvi_algorithm_1 = functools.partial(mdvi.Control1,
-                                         step_size=.1,
-                                         beta=.1,
+                                         step_size=.5,
+                                         beta=.5,
                                          initial_r_bar=0.,
                                          threshold=.1)
     algorithm_constructors.append(mdvi_algorithm_1)
     mdvi_algorithm_2 = functools.partial(mdvi.Control2,
-                                         step_size=.1,
-                                         beta=.1,
+                                         step_size=1.,
+                                         beta=1.,
                                          initial_r_bar=0.,
-                                         threshold=.1)
+                                         threshold=.01)
     algorithm_constructors.append(mdvi_algorithm_2)
   if FLAGS.rvi:
     rvi_algorithm = functools.partial(rvi.Control,
@@ -132,7 +165,7 @@ def main(argv):
     raise ValueError('No algorithms scheduled to run.')
 
   environments = []
-  problem_dtype = np.float64 if _64bit.value else np.float32
+  problem_dtype = np.float32 if _32bit.value else np.float64
   if _MDP1.value:
     environments.append(micro.create_mdp1(dtype=problem_dtype))
   if _MDP2.value:
@@ -145,6 +178,8 @@ def main(argv):
     environments.append(garet.GARET3(dtype=problem_dtype))
   if _GARET_100.value:
     environments.append(garet.GARET_100(dtype=problem_dtype))
+  if _MM1_1.value:
+    environments.append(mm1_queue.MM1_QUEUE_1(dtype=problem_dtype))
 
   if not environments:
     raise ValueError('At least one environment required.')
