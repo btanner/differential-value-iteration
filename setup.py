d0235bac27d739573d91f7caa6d72f4d96144c80
Brian Tanner <tanno@google.com>
2022-07-28 13:56:30 -0600

    Removed mini manager and pandas dependency.

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.10",
-    install_requires=["absl-py", "jaxlib", "matplotlib", "numpy", "quantecon", "pandas"]
+    install_requires=["absl-py", "jaxlib", "matplotlib", "numpy", "quantecon"]
 )
